609c5af05e3b7b58311d6aab475dd06e0e1e6128
hanmiton <hanmilton_12@outlook.com>
2017-04-26 01:18:19 -0500

    cambiando puero par deply

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ var app = express();
  */
 app.use('/', express.static(__dirname + '/static')); 
 
-var server = app.listen(3000, function () {
-  console.log('Listening on localhost:3000');
+var server = app.listen(5000, function () {
+  console.log('Listening on localhost:5000');
 });
